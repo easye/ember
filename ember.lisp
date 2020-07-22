ebc1e340cce064fa88dfc693f77ec6b41beccc02
Mark <evenson.not.org@gmail.com>
2020-07-22 20:58:32 +0200

    Restore compilation

--- ember.lisp
+++ ember.lisp
@@ -1,18 +1,17 @@
 (in-package :ember)
-    
+
+#|  
+  #1"new SparkConf()
+   .setAppName(app-name)
+   .setMaster(conf-master)"
+|#
 (defun get-conf (&key (app-name "abcl-app") (conf-master "local"))
   (let ((conf (jss:new (jss:find-java-class "org.apache.spark.sparkConf"))))
     (java:chain conf
                 ("setAppName" app-name)
                 ("setMaster" conf-master))))
-
-        
-#|  #1"new SparkConf()
-    .setAppName(app-name)
-    .setMaster(conf-master)"
-|#
     
-(defvar *spark-conf* (get-conf))o
+(defvar *spark-conf* (get-conf))
 
 (defun new-context (&key (spark-conf (get-conf)))
   (jss:new 'JavaSparkContext *spark-conf*))
